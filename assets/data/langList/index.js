512a9ae17ac9d0e5d391fee892f2965a1faef367
Patrick Divine <pddivine@Patricks-MacBook-Pro.local>
2017-07-17 22:01:38 -0700

    Added reference to resource.

--- assets/data/langList/index.js
+++ assets/data/langList/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const root = require('node-root.pddivine');
+
+// From: http://www-01.sil.org/iso639-3/download.asp
 const langIso639 = fs.readFileSync(`${root}/assets/data/langList/iso-639-3_Name_Index_20170217.tab`, { encoding: 'UTF-8'});
 
 module.exports = langIso639.split('\n').reduce( (acc, cur) => {
